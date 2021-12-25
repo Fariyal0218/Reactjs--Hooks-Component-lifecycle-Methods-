4e9b3164e27d10c3b3955be44a93d25fb01b3ba0
Fariyal0218 <fariyalshaikh5@gmail.com>
2021-12-25 18:21:37 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Form from './component/Form';
+import ParentComponent from './component/ParentComponent';
+import Register from './component/Register';
+import Success from './component/Success';
+import PropsDemo from './component/PropsDemo';
+import ComponentLifeCycle from './component/ComponentLifeCycle';
+import UseEffectHook from './component/UseEffectHook';
+import StateDemo1 from './component/StateDemo1';
+import StateDemo2 from './component/StateDemo2';
+import FormData from './component/FormData';
+import ReactServices from './component/ReactServices';
+import PostRequest from './component/PostRequest';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<App/>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/reactrouter/Services.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Services() {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>This is Services Components</p>
+        </div>
+    )
+}
